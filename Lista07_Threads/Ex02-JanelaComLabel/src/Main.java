b94df0dc7a006e2c69fc0fb6d9b71d7f0a4cf9ae
Gustavo Barbosa <gustavosb64@gmail.com>
2021-06-25 16:51:24 -0300

    Lista07-exercicio02: completo

--- Lista07_Threads/Ex02-JanelaComLabel/src/Main.java
+++ Lista07_Threads/Ex02-JanelaComLabel/src/Main.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String args[]) {
+
+        ArrayList<ThreadJFrame> arrayTJF = new ArrayList<ThreadJFrame>();
 
         try {
             BufferedReader LEITOR_ENTRADA_PADRAO = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +21,24 @@ public class Main {
                 }
                 if (userInput.compareTo("new") == 0) {
                     ThreadJFrame tJF = new ThreadJFrame();
+                    arrayTJF.add(tJF);
+
                     tJF.start();
+                }
+                if (userInput.compareTo("reset") == 0) {
+                    for ( ThreadJFrame t : arrayTJF ){
+                        t.reset();
+                    }
+                }
+                if (userInput.compareTo("bold") == 0) {
+                    for ( ThreadJFrame t : arrayTJF ){
+                        t.bold();
+                    }
+                }
+                if (userInput.compareTo("close") == 0) {
+                    for ( ThreadJFrame t : arrayTJF ){
+                        t.interrupt();
+                    }
                 }
             }
         } catch (IOException ex) {
--- Lista07_Threads/Ex02-JanelaComLabel/src/ThreadJFrame.java
+++ Lista07_Threads/Ex02-JanelaComLabel/src/ThreadJFrame.java
@@ -1,24 +1,29 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 class ThreadJFrame extends Thread{
+    private int rand;
+    private JFrame jfTela;
+    private JLabel jlTexto;
 
     public void run(){
         //Definindo estado inicial como 0
-        int rand = 0; 
+        rand = 0; 
 
         try{
 
-            JFrame jfTela = new JFrame();
+            jfTela = new JFrame();
+
             jfTela.setSize(new Dimension(200, 200));
             jfTela.setLayout(new BorderLayout());
             jfTela.setVisible(true);
 
-            JLabel jlTexto = new JLabel();
+            jlTexto = new JLabel();
             while(true){
 
                 jlTexto.setText(""+rand);
@@ -32,10 +37,18 @@ class ThreadJFrame extends Thread{
                 rand = new Random().nextInt(100000);
             }
 
-        }catch(InterruptedException e){
-            
-        }
+        }catch(InterruptedException e){ }
 
+    }
+
+    public void reset(){
+        this.jlTexto.setText("0");
+        this.jfTela.add(jlTexto);
+    }
+
+    public void bold(){
+        this.jlTexto.setFont(jlTexto.getFont().deriveFont(jlTexto.getFont().getStyle() ^ Font.BOLD));
+        this.jfTela.add(jlTexto);
     }
 
 }
